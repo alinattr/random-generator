36ad52ae5e4c95a6990f3725bf5be29a8e36e163
Alina Fedorova <87044568+alinattr@users.noreply.github.com>
2021-10-21 15:04:08 +0300

    Add files via upload

--- untitled5/app.js
+++ untitled5/app.js
@@ -1,11 +1,12 @@
 var string_inner_container = document.getElementById('string_inner_container')
-string_inner_container.innerHTML = 'Ticket ID: ' + create_random_string(6)
+string_inner_container.innerHTML = 'Ticket ID: ' + create_random_string(7)
 
 function create_random_string (string_length) {
     var random_string = ' ';
-    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz'
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     for (var i, i = 0; i < string_length; i++) {
         random_string += characters.charAt(Math.floor(Math.random() * characters.length));
     }
     return random_string
 }
+
